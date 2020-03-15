0fa19d368ca6bc8f8d6e9350e864b9717ed6f667
JvW17 <jvwijk17@gmail.com>
2020-03-15 17:01:49 +0100

    Klok 15-03-2020
    De klok is nog niet af. Ik heb ervoor gezorgd dat de klok responsive is. Wordt vervolgd.

--- scripts/test2.js
+++ scripts/test2.js
@@ -3,12 +3,13 @@ var uren = d.getHours();
 
 if (uren >= 18){
   text = "Het is avond.";
-  text2 = "Tijd voor wat ontspanning.";
+  text2 = "Tijd voor ontspanning.";
 }
 
 else if (uren >= 12){
   text = "Het is middag.";
-  text2 = "Maak er een productieve dag van.";
+  text2 = "Geniet van je dag!";
+  // document.body.background = "afbeeldingen/ruimte avond.jpg";
 }
 
 else if (uren >= 6){
@@ -18,7 +19,25 @@ else if (uren >= 6){
 
 else {
   text = "Het is nacht.";
-  text2 = "Ga maar lekker slapen. Zzz...";
+  text2 = "Ga maar lekker slapen.";
 }
 document.getElementById("tijdTesten").innerHTML = text;
 document.getElementById("tijdTestenOnderschrift").innerHTML = text2;
+
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10)
+{
+    dd='0'+dd;
+}
+
+if(mm<10)
+{
+    mm='0'+mm;
+}
+today = dd+'-'+mm+'-'+yyyy;
+document.getElementById("datum").innerHTML = today;
